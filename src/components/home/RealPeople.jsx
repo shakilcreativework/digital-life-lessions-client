e314d51a68c521f44e5f8b49991bf586c08f02c5
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-06-19 15:09:02 +0600

    real people left area complete

--- src/components/home/RealPeople.jsx
+++ src/components/home/RealPeople.jsx
@@ -5,10 +5,10 @@ const RealPeople = () => {
     return (
         <section className="py-20 md:py-24">
             <Container>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 md:gap-4 lg:gap-6">
-                    <div className="lg:col-span-2 space-y-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 md:gap-4 lg:gap-6">
+                    <div className="sm:col-span-2 md:col-span-3 lg:col-span-2 space-y-4">
                         <div>
-                            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-foreground leading-tight">Real People. <br /> Real Impact.</h1>
+                            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-foreground leading-tight">Real People. <br className="hidden md:hidden lg:block" /> Real Impact.</h1>
                         </div>
                         <p className=" lg:text-lg xl:text-2xl text-muted">Join a global community that <br className="hidden xl:inline-flex" /> learns, grows, and supports <br className="hidden xl:inline-flex" /> each other every day.</p>
                         <div className="grid grid-cols-2 sm:grid-cols-4 gap-6 md:gap-4 mt-10">
