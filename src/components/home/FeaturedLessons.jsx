7521a64ecab5befc3f510153f1dfc21277e35ec7
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-06-22 22:19:16 +0600

    Now, grid fixed for all devices

--- src/components/home/FeaturedLessons.jsx
+++ src/components/home/FeaturedLessons.jsx
@@ -56,7 +56,7 @@ const FeaturedLessons = () => {
                         <EmptyState />
                     </div>
                 ) : (
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 ">
                         {lessons.map((item) => (
                             <LessonCard key={item._id} lesson={item} />
                         ))}
